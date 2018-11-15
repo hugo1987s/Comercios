8e1d6fb29e81a09da6d08f5bb169bcf37c20aaf6
Hugo <hugoschreiber1@gmail.com>
2018-11-15 17:27:15 -0300

    Add files via upload
    Se agregan popup a los markers.
    Se cambia la manera de extraer los datos del json de comercios.
    Se renombran los json

--- scripts/productos.js
+++ scripts/productos.js
@@ -29,69 +29,64 @@ function buscarArticulos(valorABuscar) {
 
     borrarCapas();
 
-    var latitud = [0];
-    var longitud = [0];
+    var latitud = [];
+    var longitud = [];
     var index = 0;
+
+    var comercios = [];
 
     $("#divMapa").css("display", "block");
     $("#divGrilla").css("display", "block");
 
-    $.getJSON("Json/articlesData.json", function (data) {
-
+    $.getJSON("Json/Comercios.json", function (data) {
         $("#listaNegocios").empty();
-
         $.each(data, function (index, value) {
             if (value.Categoria.toLowerCase().indexOf(valorABuscar.toLowerCase()) >= 0) {
+                
                 var li = $('<li>');
-                li.append(value.Vendedor + " (" + value.Categoria + ")");
+                //li.append(value.Vendedor + " (" + value.Categoria + ")");
+                li.append(armarPopup(value));
                 $("#listaNegocios").append(li);
 
                 latitud.push(value.Latitud);
                 longitud.push(value.Longitud);
+                
+                comercios.push(value);
+                //mostrarComercios(value);
             }
         });
-
-        /*
-        $("#tblProductos > tbody").html("");
-        $.each(data, function (index, value) {
-
-            // if (  value.Categoria == valorABuscar) {
-            if (value.Categoria.toLowerCase().indexOf(valorABuscar.toLowerCase()) >= 0) {
-                $('#tblProductos > tbody:last').append('<tr><td>'
-                    + value.Categoria + '</td><td>'
-                    + value.Articulo + '</td><td>'
-                    + value.Precio + '</td><td>'
-                    + value.Vendedor + '</td></tr>');
-
-                latitud.push(value.Latitud);
-                longitud.push(value.Longitud);
-            }
-
-
-        });
-*/
-        mostrarComercios(latitud, longitud);
-
+        //mostrarComercios(latitud, longitud);
+        mostrarComercios(comercios);
     });
 }
 
 
 
+function mostrarComercios(listaComercios) {
+
+    var cluster = L.markerClusterGroup();
+
+    for (i = 0; i < listaComercios.length; i++) {
+        cluster.addLayers([
+            L.marker([listaComercios[i].Latitud, listaComercios[i].Longitud]).bindPopup(armarPopup(listaComercios[i]))
+        ])
+    }
+    cluster.addTo(map);
+    map.addLayer(cluster);
+}
+
+function armarPopup(datosComercio)
+{
+    return '<h4>'+ datosComercio.Vendedor + '</h4>' +
+    '<b>Domicilio:</b> ' + datosComercio.Domicilio +
+    '<br><b>Telefono:</b> ' + datosComercio.Telefono +
+    '<br><b>Horario:</b> ' + datosComercio.Horario +
+    '<hr>';
+}
+
+/*
 function mostrarComercios(latitud, longitud) {
-
-
-
     var ungsLocation = [-34.5221554, -58.7000067];
-
-    /*  L.polygon([
-        L.latLng(-34.515594, -58.705654),
-        L.latLng(-34.523503, -58.714062),
-        L.latLng(-34.519177, -58.719890),
-        L.latLng(-34.511089, -58.711374),
-        L.latLng(-34.514062, -58.707909),
-        L.latLng(-34.513824, -58.707584)
-    ]).addTo(map);
-    */
 
     var ungsMarker = L.marker(ungsLocation);
     ungsMarker.addTo(map);
@@ -109,3 +104,6 @@ function mostrarComercios(latitud, longitud) {
     cluster.addTo(map);
     map.addLayer(cluster);
 }
+*/
+
+//L.marker([latitud[i], longitud[i]]).bindPopup(info);
